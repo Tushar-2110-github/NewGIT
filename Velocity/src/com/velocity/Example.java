cf5c9f6bb1112812b88d6fb0c7aaa6896ed1de0b
Tushar <tushar.tusharbramhankar21@gmail.com>
2023-08-30 14:33:20 +0530

    Adding the values of a and b

--- Velocity/src/com/velocity/Example.java
+++ Velocity/src/com/velocity/Example.java
@@ -1,10 +1,12 @@
 package com.velocity;
 
 public class Example {
-public static void main(String[] args) {
-	int a =10;
-	int b=20;
-	int c=a+b;
-	System.out.println("addition is " + c);
-}
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Value of a is : " +a);
+		System.out.println("Value of b is : " + b);
+		System.out.println("addition is " + c);
+	}
 }
